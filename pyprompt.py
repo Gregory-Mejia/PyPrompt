4bbd5010b50bd8f257cb3319e719a1c75db1249e
Gregory Mejia <160346215+Gregory-Mejia@users.noreply.github.com>
2025-04-17 13:58:18 -0700

    Added type annotation and pep-8 formatting
    As said in the title.

---
+++ pyprompt.py
@@ -0,0 +1,25 @@
+"""
+
+    Date: 4/17/2025
+    Purpose:
+    Create a library that allows for safe user console prompting.
+
+"""
+
+# -- Import Typing Lib -- #
+
+from typing import TypeVar
+
+# -- Variables -- #
+
+T = TypeVar("type")  # For the parameter type definition and return-type
+
+# -- Functions -- #
+
+
+def prompt_console(text: str, _type: T) -> T:
+    pass
+
+
+def a():
+    ...
