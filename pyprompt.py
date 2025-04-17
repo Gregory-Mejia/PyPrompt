32f98196317476526aef08fcbda68f8fa1a8317f
Gregory Mejia <160346215+Gregory-Mejia@users.noreply.github.com>
2025-04-17 14:34:23 -0700

    Added documentation and boolean check.
    Included the exception statement that houses a check for a ValueError, which is not yet handled. Inside the try block, check and handle if the _type is a bool for its special properties.

--- pyprompt.py
+++ pyprompt.py
@@ -18,4 +18,41 @@ T = TypeVar("type")  # For the parameter type definition and return-type
 
 
 def prompt_console(text: str, _type: T) -> T:
-    pass
+    """
+        This function attempts to resolve the user's input to the type provided
+        Takes in 'text' as a string and '_type' as a type and returns said type
+
+        'text' is used to prompt the user
+        '_type' is used to continuously check for the required input
+    """
+
+    # Get the input from the user
+    u_inp = input(text)
+
+    # Exception block to try and resolve the type
+    try:
+        # Check to handle the special case for boolean type resolving
+        if (_type == bool):
+            # Lower the string to not have to check for more cases
+            u_inp = u_inp.lower()
+
+            # Use a long if statement to check if it's any variant of true
+            if ((u_inp == "true") or (u_inp == "yes") or (u_inp == "y")):
+                return True
+            # Check for all variants of the false boolean
+            elif ((u_inp == "false") or (u_inp == "no") or (u_inp == "n")):
+                return False
+            # Default to a reprompt for invalid input
+            else:
+                # Do a recursive call-back for reprompting
+                print(f"Invalid input for type: {_type}\nTry again.")
+                prompt_console(text=text, _type=_type)
+        else:
+            # Otherwise, return the type given
+            u_inp = _type(u_inp)
+            return u_inp
+    except ValueError as ve:
+        print(ve)
+
+    print(type(u_inp))
+    print(u_inp)
